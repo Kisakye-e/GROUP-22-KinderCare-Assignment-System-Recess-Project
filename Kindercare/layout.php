00393e6ac743b83d94e71b3e9d14878a5f711dfd
Esther Kisakye <eng.kisakye@gmail.com>
2022-02-01 16:17:25 +0300

    Added register pupil functionality and home page

--- Kindercare/layout.php
+++ Kindercare/layout.php
@@ -18,20 +18,20 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-      <a class="nav-link" href="/home">Home<span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="home.php">Home<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/registerpupil">Register Pupil</a>
+        <a class="nav-link" href="registerPupil.php">Register Pupil</a>
       </li>
       
       <li class="nav-item">
-        <a class="nav-link" href="/addassignment">Add Assignment</a>
+        <a class="nav-link" href="addassignment.php">Add Assignment</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/activationrequest">View Activation Requests</a>
+        <a class="nav-link" href="activationRequests.php">View Activation Requests</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/assignmentactivity">Assignment Activity</a>
+        <a class="nav-link" href="assignmentActivity.php">Assignment Activity</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
