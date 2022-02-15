902cdd982e31e8ab714daf4a0c3b840cc71ecd2a
Daniella Caden <daniella.wwcd@gmail.com>
2022-02-15 15:04:22 +0300

    Update editAssignment.php

--- editAssignment.php
+++ editAssignment.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 include_once 'layout.php'; 
     $id =$_GET['id'];  
     $queryZ = "SELECT * FROM submittedassignment where assignmentnumber = '$id'";
@@ -16,7 +16,7 @@ include_once 'layout.php';
             <div class="page-breadcrumb bg-white">
                 <div class="row align-items-center">
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">Edit pupil details</h4>
+                        <h4 class="page-title">Edit Assignment</h4>
                     </div>
                     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                        
