4d4b7c586f1a0ff9e50f0610c2c2cced8d0d5aa5
Esther Kisakye <eng.kisakye@gmail.com>
2022-02-15 12:14:16 +0300

    Reconnected to localhost

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$servername='dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-$username='xmqql08wsxi3k0vp';
-$password='kxij7jf50jee9hig';
-$dbname = "oywcou8i94atn3y7";
+$servername='localhost';
+$username='root';
+$password='';
+$dbname = "kindercare";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
    die('Could not Connect My Sql:' .mysql_error());
