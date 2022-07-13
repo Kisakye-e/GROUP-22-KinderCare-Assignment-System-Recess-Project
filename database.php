05e07548128b055bf42da6b9c7b28352cdf6fe2d
Esther Kisakye <78255316+Kisakye-e@users.noreply.github.com>
2022-07-13 19:12:52 +0300

    Connected to a new database

--- database.php
+++ database.php
@@ -1,8 +1,9 @@
 <?php
-$servername='dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-$username='xmqql08wsxi3k0vp';
-$password='kxij7jf50jee9hig';
-$dbname = "oywcou8i94atn3y7";
+$servername='fdb32.awardspace.net';
+$username='4138731_kindercare';
+$password='#hZVpv5BYymk-Ua';
+$dbname = "
+4138731_kindercare";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
    die('Could not Connect My Sql:' .mysql_error());
